9d10ee60595b45e1aee787bee88d38f9f042856d
edrill <edrillb@gmail.com>
2024-12-30 20:22:10 +0800

    Fixed some padding issues

--- src/components/BestSellerGrid.tsx
+++ src/components/BestSellerGrid.tsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 export const BestSellerGrid: React.FC = () => {
   return (
-    <div className="w-full h-full text-white grid md:grid-cols-2 lg:grid-cols-3 gap-12 md:py-5 md:px-20 items-center">
+    <div className="w-full min-h-screen text-white grid md:grid-cols-2 lg:grid-cols-3 gap-12 py-5 px-20 items-center">
         <Card image="/images/HDrill.png" price="79" name="Hotdog ni Edrill" />
         <Card image="/images/HDRimoo.png" price="69" name="Hotdog ni Rimoo" />
         <Card image="/images/HDLans.png" price="59" name="Hotdog ni Lans" />
