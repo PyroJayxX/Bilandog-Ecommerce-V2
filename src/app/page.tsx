85410b6a15a4bb8eb501b42c80626e4ebbee976d
edrill <edrillb@gmail.com>
2024-12-30 20:32:09 +0800

    Minor bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,12 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-full h-full p-8" >
+      <div className="w-full h-full p-8 xl:my-5" >
         <div className="flex relative">
-          <h1 className="text-4xl font-bold text-center italic font-serif flex-grow text-white">Best Sellers</h1>
+          <h1 className="text-4xl xl:text-5xl font-bold text-center italic font-serif flex-grow text-white">Best Sellers</h1>
           <Image src="/icons/cart_icon.svg" alt="Arrow" width={40} height={40} className="m-auto md:absolute md:right-10 filter invert" />
         </div>
-        <hr className="border-[#A4A4A4] my-8" />
+        <hr className="border-[#A4A4A4] md:my-8 xl:my-10" />
         <BestSellerGrid />
       </div>
     </div>
