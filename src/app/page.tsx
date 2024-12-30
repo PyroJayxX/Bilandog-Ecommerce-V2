5ed65f7e7ede1e4551178040fb0ff47f90ff0710
edrill <edrillb@gmail.com>
2024-12-30 15:26:49 +0800

    Added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
 import Image from "next/image";
 import TopNav from "../components/TopNav";
 import Hero from "../components/Hero";
+import BestSellerGrid from "../components/BestSellerGrid";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen overflow-x-hidden"> 
-    
+      <TopNav />
+      
       <div className="relative flex flex-col w-full h-full">
-        <Image src="/images/hero.png" alt="hero" layout="fill" objectFit="cover" className="filter brightness-50" />
-        <div className="absolute top-0 left-0 w-full h-full flex flex-col">
-          <TopNav />
+        <div className="absolute top-0 left-0 w-full h-full grid">
           <Hero />
         </div>
       </div>
 
       <div className="w-screen h-full bg-white p-8" >
-        <h1 className="text-4xl font-bold text-center">Welcome to our site</h1>
+        <div className="flex relative">
+          <h1 className="text-4xl font-bold text-center italic font-serif flex-grow">Best Sellers</h1>
+          <Image src="/icons/cart_icon.svg" alt="Arrow" width={40} height={40} className="absolute right-10" />
+        </div>
+        <hr className="border-[#A4A4A4] my-8" />
+        <BestSellerGrid />
       </div>
     </div>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+
 export const Hero: React.FC = () => {
-
     return (
-        <div className='flex flex-col w-full h-full p-32 items-start'>
-          <div className='flex-grow text-white font-serif text-6xl flex flex-col p-8 justify-center items-center' style={{ textShadow: "2px 2px 4px rgba(16, 16, 16, 0.5)" }}>
-            <h1 className='p-2 py-2'> 
-              Hotdog <span>anywhere</span>,
-            </h1>
-            <h1 className='p-2 py-2'> 
-              <span>Wherever</span> you are.
-            </h1>
-          </div>
-
-          <div className='flex justify-center p-7 flex'>
-            <button className='bg-[#6C1814] text-white text-xl p-3 m-4 rounded-2xl hover:bg-[#926F10] hover:text-white font-serif shadow-xl' style={{ textShadow: "2px 2px 4px rgba(16, 16, 16, 0.5)" }}>
-              <span className="px-6">Shop Now</span>
-            </button>
-            <Link href='/about' className='p-6 pl-3 font-sans font-light text-normal text-xl text-[#FFE2C4]' style={{ textShadow: "2px 2px 4px rgba(20, 20, 20, 0.5)" }}>
-                Learn More
-            </Link>
-          </div>
+        <div className = 'flex flex-col flex-grow flex-shrink spacey-3 justify-center md:justify-end items-center md:items-start text-white font-serif text-5xl p-10 md:text-6xl md:p-20' style={{ backgroundImage: "url('/images/hero.png')", backgroundSize: "cover", backgroundPosition: "center", position: "relative" }}>
+            <div className = "flex flex-grow flex-shrink spacey-3 text-xl" style={{ zIndex:0, position: "absolute", top: 0, left: 0, right: 0, bottom: 0, backgroundColor: "rgba(0, 0, 0, 0.7)"}}></div>
+            <h1 className = "relative z-10" style={{textShadow: "2px 2px 4px rgba(16, 16, 16, 0.8)"}}>Hotdogs anytime,</h1>
+            <h1 className = "relative z-10" style={{textShadow: "2px 2px 4px rgba(16, 16, 16, 0.8)"}}>Anywhere you are.</h1>
+            <div className="relative z-10 flex items-center gap-4">
+            <button className='bg-[#6C1814] text-white text-xl px-3 py-1 my-8 rounded-xl md:text-3xl md:px-6 md:py-2 md:my-16 md:rounded-2xl hover:bg-[#926F10] hover:text-white font-serif shadow-xl'
+                style={{ textShadow: "2px 2px 4px rgba(16, 16, 16, 0.5)" }}>Shop Now</button>
+              <Link href="/learn-more">
+                <h1 className='text-[#FFE2C4] text-xl md:text-3xl font-serif hover:underline'>Learn More</h1>
+              </Link>
+            </div>
         </div>
     )
 }
