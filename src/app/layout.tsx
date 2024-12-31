135ebc3bf031d0a6f900092ddab047f14f2a5727
edrill <edrillb@gmail.com>
2025-01-01 03:06:55 +0800

    Added meta headers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -24,10 +25,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      {children}
       </body>
     </html>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <>
         <hr />
-        <div className="w-screen h-auto p-8 xl:my-5 mx-10 px-32 bg-[#171717] text-white grid grid-cols-3 gap-10 justify-space items-start font-serif">
+        <div className="w-screen h-auto p-8 xl:my-5 md:mx-10 px-32 bg-[#171717] text-white grid grid-cols-3 gap-10 justify-space items-start font-serif">
             <div className="flex flex-col items-start w-full mx-auto">
             <h1 className="text-[#F05A19] text-xl" style={{textShadow: '1px 0px 1px #F05A19'}}> About </h1>
             <Link href="/">About Us</Link>
@@ -27,7 +27,7 @@ export const Footer = () => {
             <div className="md:flex space-x-4">
                 <Image src="/icons/icon_twitter.svg" alt="Twitter" width={30} height={30} />
                 <Image src="/icons/icon_fb.svg" alt="Facebook" width={30} height={30} />
-                <Image src="/icons/icon_ig.svg" alt="Instagram" width={30} height={30} />
+                <Image src="/icons/icon_ig.svg" alt="Instagram" width={35} height={35} />
                 <Image src="/icons/icon_tiktok.svg" alt="Instagram" width={30} height={30} />
             </div>
             <h1>@Bilandog_Corp</h1>
